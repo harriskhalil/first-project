5b6ad8366105caf941fe8f4736397d67c680d9d7
haris <haris@nodesol.com>
2021-04-11 18:36:08 +0500

    trying to learn

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test/{tests}', function ($post) {
+    $posts=['my-first-post'=>'this is my first post',
+            'my-second-post'=>'this is my second post'
+            ];
+    return view('post',[
+        'post'=>$posts[$post]
+    ]);
+});
